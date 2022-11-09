d948b3293a9b9097ec38e84709d777c575db704d
GauravSingh46 <113342946+GauravSingh46@users.noreply.github.com>
2022-11-09 07:06:40 +0530

    Update DoctorDao.java

--- src/main/java/com/example/HealthProject/dao/DoctorDao.java
+++ src/main/java/com/example/HealthProject/dao/DoctorDao.java
@@ -9,6 +9,6 @@ import java.util.List;
 public interface DoctorDao extends JpaRepository<Doctor,Long> {
 
 
-    @Query(value="SELECT * FROM doctor WHERE doctor.first_name LIKE CONCAT('%',:query, '%') OR doctor.middle_name LIKE CONCAT('%',:query, '%') OR doctor.last_name LIKE CONCAT('%',:query, '%')",nativeQuery = true)
+    @Query(value="SELECT * FROM doctor WHERE doctor.name LIKE CONCAT('%',:query, '%')",nativeQuery = true)
     List<Doctor> doctorBySearch(String query);
 }
